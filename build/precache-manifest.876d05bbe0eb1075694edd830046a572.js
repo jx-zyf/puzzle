affbdacbba76b1e7b79b96a208ad9cb91e692c47
jx-zyf <656874691@qq.com>
2018-10-24 17:29:39 +0800

    fix path

---
+++ build/precache-manifest.876d05bbe0eb1075694edd830046a572.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2972836d66ac77bb9df9",
+    "url": "./static/js/main.2972836d.chunk.js"
+  },
+  {
+    "revision": "ea212b9ed15b8aa59b61",
+    "url": "./static/js/1.ea212b9e.chunk.js"
+  },
+  {
+    "revision": "2972836d66ac77bb9df9",
+    "url": "./static/css/main.ff5e11da.chunk.css"
+  },
+  {
+    "revision": "5b95544a0b2902553bbebd2eac67f352",
+    "url": "./index.html"
+  }
+];
