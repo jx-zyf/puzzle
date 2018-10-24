bad708272edf9bb6a8eab5ecd6b598cbf04b454e
jx-zyf <656874691@qq.com>
2018-10-24 17:21:16 +0800

    fixed

---
+++ build/precache-manifest.2e38bbbdb43919a96e0e16d8c1479bd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "58bbd9534dd40d9a811a",
+    "url": "/static/js/main.58bbd953.chunk.js"
+  },
+  {
+    "revision": "ea212b9ed15b8aa59b61",
+    "url": "/static/js/1.ea212b9e.chunk.js"
+  },
+  {
+    "revision": "58bbd9534dd40d9a811a",
+    "url": "/static/css/main.ff5e11da.chunk.css"
+  },
+  {
+    "revision": "4b4e36254906a4fa1ccb3d6d7beed664",
+    "url": "/index.html"
+  }
+];
